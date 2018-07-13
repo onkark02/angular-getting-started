ac1d54d15729e6df81d7cdd9d05ff59aced97254
Kumbhar <onkar.kumbhar@siemens.com>
2018-07-13 17:03:49 +0530

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import {ConvertToSpacesPipes} from './shared/convert-to-spaces.pipes';
+import { StarComponent } from './shared/star.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipes,
+    StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
